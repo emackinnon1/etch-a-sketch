8acbe9ed6c6d1cf4e2f065a0b4551b648eeaef0e
emackinnon1 <emackinnon100390@gmail.com>
2019-12-05 13:05:48 -0500

    change name of clear function to reset, is working now

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
-const container = document.getElementById('container');
+window.onload = makeGrid(16, 16);
+
+
+
 
 function makeGrid(rowNum, colNum) {
+  const container = document.getElementById('container');
   for (i = 0; i < rowNum; i++) {
         for (j = 0; j < colNum; j++) {
             let newCell = document.createElement("div");
@@ -9,11 +13,12 @@ function makeGrid(rowNum, colNum) {
           }
         }
         hover();
+
+
 }
 
-
 function hover() {
-  const cell = document.querySelectorAll(".cell");
+  const cell = document.querySelectorAll(".cell")
   cell.forEach((item) => {
     item.addEventListener('mouseenter', (e) => {
       e.target.style.backgroundColor = '#000000';
@@ -21,17 +26,11 @@ function hover() {
   });
 }
 
-function clear() {
-  const clear = document.getElementByClassName('clear');
+
+const clear = document.getElementById('clear');
+clear.addEventListener("click", function (e) {
   const cell = document.querySelectorAll(".cell");
-
-  clear.addEventListener('click', function (e){
-    cell.forEach(item => {
-      e.target.style.backgroundColor = 'white';
+    cell.forEach((cell) => {
+      cell.style.background = '#ffffff';
     });
-  });
-}
-
-
-
-makeGrid(16, 16);
+});
