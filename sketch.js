6e2fa59c61f73e3d9810d20a2655d59021f5a42d
emackinnon1 <emackinnon100390@gmail.com>
2019-12-11 12:25:12 -0500

    Clean up makeNewGrid function and event listener

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
-window.onload = makeGrid(16, 16);
-
-
+window.onload = makeGrid(16);
 
 
 function makeGrid(num) {
@@ -8,10 +6,10 @@ function makeGrid(num) {
   let cols = container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   let rows = container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
   for (i = 0; i < num * num; i++) {
-            let newCell = document.createElement("div");
-            newCell.className = "cell";
-            container.appendChild(newCell);
-          }
+      let newCell = document.createElement("div");
+      newCell.className = "cell";
+      container.appendChild(newCell);
+    }
         hover();
 }
 
@@ -30,12 +28,12 @@ function clear() {
     cell.style.background ='#ffffff';
   });
 }
+
+function makeNewGrid() {
+  let input = window.prompt("How many squares?");
+  makeGrid(input);
+  clear();
+}
+
 document.getElementById('clear').addEventListener("click", clear);
-
-
-const newGrid = document.getElementById('newGrid');
-newGrid.addEventListener("click", function (e) {
-    clear();
-    let input = window.prompt("How many squares?");
-    makeGrid(input, input);
-});
+document.getElementById('newGrid').addEventListener("click", makeNewGrid);
