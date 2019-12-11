34123bbf50b4ac3a7e797683a3bc85143d58fbbe
emackinnon1 <emackinnon100390@gmail.com>
2019-12-11 11:58:07 -0500

    Create clear function for makeGrid

--- sketch.js
+++ sketch.js
@@ -3,23 +3,20 @@ window.onload = makeGrid(16, 16);
 
 
 
-function makeGrid(rowNum, colNum) {
+function makeGrid(num) {
   const container = document.getElementById('container');
-  let cols = container.style.gridTemplateColumns = `repeat(${colNum}, 1fr)`;
-  let rows = container.style.gridTemplateRows = `repeat(${rowNum}, 1fr)`;
-  for (i = 0; i < rowNum * colNum; i++) {
+  let cols = container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  let rows = container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+  for (i = 0; i < num * num; i++) {
             let newCell = document.createElement("div");
             newCell.className = "cell";
             container.appendChild(newCell);
-
-        }
+          }
         hover();
-
-
 }
 
 function hover() {
-  const cell = document.querySelectorAll(".cell")
+  const cell = document.querySelectorAll(".cell");
   cell.forEach((item) => {
     item.addEventListener('mouseenter', (e) => {
       e.target.style.backgroundColor = '#000000';
@@ -27,18 +24,18 @@ function hover() {
   });
 }
 
-
-const clear = document.getElementById('clear');
-clear.addEventListener("click", function (e) {
+function clear() {
   const cell = document.querySelectorAll(".cell");
-    cell.forEach((cell) => {
-      cell.style.background = '#ffffff';
-    });
+  cell.forEach((cell) => {
+    cell.style.background ='#ffffff';
+  });
+}
+document.getElementById('clear').addEventListener("click", clear);
 
-});
 
 const newGrid = document.getElementById('newGrid');
 newGrid.addEventListener("click", function (e) {
+    clear();
     let input = window.prompt("How many squares?");
     makeGrid(input, input);
 });
